95b8e85b40bb5a4b62a959e6220e878b9742d0d6
komalkam <136676732+komalkam@users.noreply.github.com>
2023-06-15 23:10:31 +0530

    Add files via upload

---
+++ k.py
@@ -0,0 +1,95 @@
+#num1 = "komal"
+#print(num1) 
+
+#num1 = 23
+#print(num1)
+#print(id(num1))
+#num2="India is my country,I love my country"
+#print(num2)
+#num2='India is my country,I love my country'
+#print(num2)
+
+#num1  ="'python'is coding language"
+#print(num1)
+
+
+# Python complex() function example  
+# Calling function  
+
+#
+
+
+     
+    
+#list1 =[1,2,3]
+#list2 = list1
+#list1[0] = 4
+
+
+#s= input()
+#count=0
+#print("input string is:", s)
+#print("initial count:", count)
+#for i in s:
+ #   count+=1
+  #  print(i,count)
+
+#print("number of element is:", count)    
+
+
+  #  lenth= number of elements/characters-len()
+
+
+
+#s= input()
+#len_s=len(s)
+#print(len_s)
+
+
+#lenght alwyas starts number 1.
+
+#lenght is used in all data types
+
+
+
+
+
+
+#sample input=341209
+
+
+#s=input()
+
+#num=s[::-1]
+#print(int(num))
+
+
+#lower=int(input("enter lower limit:"))
+#upper=int(input("enter upper limit:"))
+
+#for i in range(lower, upper + 1):
+ #  if i % 2 == 0:
+  #     print(i)
+
+# list=[1,5,8,0,3,5,7,4]
+# odd_count=0
+# even_count=0
+# for i in list:
+#     if i % 2 == 0:
+#        even_count+= 1
+#     else: 
+#        odd_count+=1
+# print(even_count)
+# print(odd_count)
+
+
+
+# a, b = 1, 1
+# while a <= 50:
+#     print(a, end=" ")
+#     a, b = b, a + b
+
+
+word = input("Enter a word: ")
+reversed_word = word[::-1]
+print("Reversed word:", reversed_word)
